bd5c19ae7f93b1ddb09fbd49a676d98747acbd71
valgn <valentino0dominguez@gmail.com>
2025-10-21 14:03:10 -0300

    ej9 y ej10

---
+++ Arquitectura/intro/plancha3.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <math.h>
+
+union FloatBit{
+    float f;
+    unsigned int u;
+};
+typedef union FloatBit FloatBit;
+
+int myisnan(FloatBit f){
+    return f.f != f.f ? 1 : 0; // hice primero este que es la consigna b xd
+}
+
+int myisnan2(FloatBit f){
+    unsigned int expo = (f.u >> 23) & 0xFF;
+    unsigned int mantisa = f.u & 0x7FFFFF;
+    return (expo == 0xFF) && (mantisa != 0); 
+}
+
+
+
+
+int main(void)
+{
+    float g = 0.0;
+    float f = 0.0 / g;
+    float h = INFINITY - INFINITY;
+
+    FloatBit f2;
+    f2.f = f;
+    printf("f: %f\n", f);
+    // ADVERTENCIA : ‘NAN ’ es una extensi ´on de GCC .
+    if (f == NAN)
+    {
+        printf("Es NAN \n");
+    }
+    if (isnan(f))
+    {
+        printf(" isNaN dice que si\n");
+    }
+    if(myisnan(f2)){
+        printf("Funciona la verga esta.\n");
+    }
+    if(myisnan2(f2)){
+        printf("Este tambien funca.\n");
+    }
+    if(isinf(h)){
+        printf("El numero es infinito\n");
+    }
+    if(h == INFINITY){
+        printf("El numero es infinito.\n");
+    }
+    printf("%f\n", h);
+    return 0;
+}
