7fd3db8facfe1ba53c9babc0f08eaf78fda38892
valgn <valen@LAPTOP-65TEIBK5.localdomain>
2026-04-19 18:28:42 +0000

    TP HAS BEEN FINISHED

--- E2/TP/tp.hs
+++ E2/TP/tp.hs
@@ -1,3 +1,4 @@
+
 import Data.Ord (comparing)
 import Data.List
 
@@ -5,6 +6,7 @@ data NdTree p = Node (NdTree p) p (NdTree p) Int | Empty deriving (Eq, Ord, Show
 
 newtype Punto2d = P2d (Double, Double) deriving (Eq, Show)
 newtype Punto3d = P3d (Double, Double, Double) deriving (Eq, Show)
+
 
 --- Definicion de la clase Punto, cada punto definido debe contar con estas funciones definidas
 class Punto p where
@@ -42,8 +44,162 @@ instance Punto Punto3d where
   coord 2 (P3d (_, _, z)) = z
 -----------------------------------------------------------------------------
 
+--- Tipo de rectangulo
+type Rect = (Punto2d, Punto2d)
 
 
+
+-----------------------------------------------------------------------------
+--- FUNCIÓN PRIVADA PARA OBTENER LA MEDIANA Y LA PRIMERA Y SEGUNDA SUBLISTAS USANDO A LA MEDIANA COMO PUNTO DE INFLEXION
+--- Mediante una lista de puntos, obtenemos la mediana y su sublista izquierda y derecha (usando la mediana como inicio y final)
+mediana :: Punto p => Int -> [p] -> ([p], p, [p])
+mediana eje p = (menoresEq, m, mayores)
+              where 
+                l = length p `div` 2
+                (primeraMitad, m:segundaMitad) = splitAt l p
+                valorMedio = coord eje m
+                mitades = primeraMitad++segundaMitad
+                menoresEq = [p | p <- mitades, coord eje p <= valorMedio]
+                mayores = [p | p <- mitades, coord eje p > valorMedio]
+
+--- FUNCIÓN PRIVADA PARA PASAR DE LISTA DE PUNTOS A UN NDTREE.
+--- Toma una lista de puntos el nivel en el que estamos y la dimension (constante)
+fromListAux :: Punto p => [p] -> Int -> Int -> NdTree p
+fromListAux [] _ _ = Empty
+fromListAux puntos lvl dim = Node (fromListAux izq (lvl+1) dim) mid (fromListAux der (lvl+1) dim) eje
+                              where 
+                                eje = mod lvl dim
+                                spuntos = sortOn (coord eje) puntos
+                                (izq, mid, der) = mediana eje spuntos
+
+--- FUNCIÓN PUBLICA PARA PASAR DE LISTA DE PUNTOS A NDTREE
+--- Toma una lista de Puntos y devuelve un NDTree
+fromList :: Punto p => [p] -> NdTree p
+fromList [] = Empty
+fromList puntos = fromListAux puntos 0 (dimension (head puntos))
+
+
+-----------------------------------------------------------------------------
+--- FUNCIÓN PRIVADA PARA INSERTAR UN PUNTO EN UN NDTREE, TOMA EL PUNTO A INSERTAR, EL NIVEL EN EL QUE ESTAMOS Y EL ARBOL EN EL QUE QUEREMOS INSERTAR
+--- Devuelve el nuevo arbol con el punto insertado
+insertarAux :: Punto p => p -> Int -> NdTree p -> NdTree p
+insertarAux punto lvl Empty = Node Empty punto Empty (mod lvl (dimension punto))
+insertarAux punto lvl (Node izq md der eje) | coord eje punto <= coord eje md   = (Node (insertarAux punto (lvl+1) izq) md der eje)
+                                            | otherwise                         = (Node izq md (insertarAux punto (lvl+1) der) eje)
+
+--- FUNCIÓN PUBLICA PARA INSERTAR UN PUNTO EN UN NDTREE, TOMA EL PUNTO A INSERTAR Y EL ARBOL EN EL QUE QUEREMOS INSERTAR
+--- Devuelve el nuevo arbol con el punto insertado
+insertar :: Punto p => p -> NdTree p -> NdTree p
+insertar punto arbol = insertarAux punto 0 arbol
+
+
+-----------------------------------------------------------------------------
+--- FUNCIÓN PRIVADA PARA CALCULAR EL MAXIMO ENTRE DOS PUNTOS CON RESPECTO AL EJE DADO
+--- Devuelve el mayor punto entre los dos.
+maxCord :: Punto p => Int -> p -> p -> p
+maxCord eje p1 p2  | (coord eje p1) < (coord eje p2) = p2
+                      | otherwise = p1
+
+--- FUNCIÓN PRIVADA PARA CALCULAR EL MÍNIMO ENTRE DOS PUNTOS CON RESPECTO AL EJE DADO.
+--- Devuelve el menor punto entre los dos   
+minCord :: Punto p => Int -> p -> p -> p
+minCord eje p1 p2  | (coord eje p1) > (coord eje p2) = p2
+                      | otherwise = p1
+
+--- FUNCIÓN PRIVADA QUE BUSCA EL MÁXIMO DEL SUBÁRBOL IZQUIERDO DE UN NODO.
+--- Si el nodo es una hoja, el máximo es el punto del mismo.
+--- Si el nodo no es una hoja, el máximo se encuentra en el subárbol derecho del mismo, por lo que se busca recursivamente.
+buscarMaxIzq :: (Eq p, Punto p) => NdTree p -> Int -> p
+buscarMaxIzq (Node Empty md Empty _) _ = md
+buscarMaxIzq (Node izq md der eje) ejeImp
+  | ejeImp == eje   = case der of
+                        Empty -> md
+                        _     -> maxCord ejeImp md (buscarMaxIzq der ejeImp)
+                        
+  | otherwise       = maxCord ejeImp md (maxCord ejeImp (buscarMaxIzq der ejeImp) (buscarMaxIzq izq ejeImp))
+
+--- FUNCIÓN PRIVADA QUE BUSCA EL MÍNIMO DEL SUBÁRBOL DERECHO DE UN NODO.
+--- Si el nodo es una hoja, el mínimo es el punto del mismo.
+buscarMinDer :: (Eq p, Punto p) => NdTree p -> Int -> p
+buscarMinDer (Node Empty md Empty _) _ = md
+buscarMinDer (Node izq md der eje) ejeImp
+  | ejeImp == eje   = case izq of
+                        Empty -> md
+                        _     -> minCord ejeImp md (buscarMinDer izq ejeImp)
+  | otherwise       = minCord ejeImp md (minCord ejeImp (buscarMinDer der ejeImp) (buscarMinDer izq ejeImp))
+
+--- FUNCIÓN PRIVADA QUE SE ENCARGA DE DIVIDIR EL CASO DE ELIMINACIÓN DE UN NODO CON DOS HIJOS EN DOS CASOS, DEPENDIENDO 
+--- DE SI EL SUBÁRBOL DERECHO ES EMPTY O NO, YA QUE SI EL SUBÁRBOL DERECHO ES EMPTY, EL SUCESOR SERÁ EL MÁXIMO DEL SUBÁRBOL 
+--- IZQUIERDO, Y SI NO LO ES, EL SUCESOR SERÁ EL MÍNIMO DEL SUBÁRBOL DERECHO.
+hardCase :: (Eq p, Punto p) => NdTree p -> p -> NdTree p
+hardCase (Node izq m Empty eje) punto = let sucesor = buscarMaxIzq izq eje
+                                        in (Node (eliminar sucesor izq) sucesor Empty eje)   
+                                                                      
+hardCase (Node izq m der eje) punto   = let sucesor = buscarMinDer der eje
+                                        in (Node izq sucesor (eliminar sucesor der) eje)
+
+--- FUNCION PUBLICA PARA ELIMINAR UN PUNTO DESEADO EN UN NDTREE
+--- Si el punto es una hoja, simplemente la borramos, sino, hardcase se encarga de la logica de eliminar un nodo que tenga hijo/s
+eliminar :: (Eq p, Punto p) => p -> NdTree p -> NdTree p
+eliminar _ Empty = Empty
+eliminar punto (Node Empty m Empty eje)  
+                    | punto == m  = Empty
+                    | otherwise   = (Node Empty m Empty eje)
+eliminar punto (Node izq m der eje)  
+    | punto == m = hardCase (Node izq m der eje) punto
+    | coord eje punto <= coord eje m = (Node (eliminar punto izq) m der eje)
+    | otherwise = (Node izq m (eliminar punto der) eje)
+-----------------------------------------------------------------------------
+
+
+-----------------------------------------------------------------------------
+--- Funcion que toma un rectangulo y devuelve una 4-upla con las coordenadas ordenadas de menor a mayor (x1,y1) es el punto inferior izquierdo
+--- (x2,y2) es el punto superior derecho
+ordenarRect :: Rect -> (Double, Double, Double, Double)
+ordenarRect (P2d (x, y), P2d (a, b)) = (min x a, min y b, max x a, max y b)
+
+--- Funcion que determina si un punto dado pertenece a un rectangulo
+inRegion :: Punto2d -> Rect -> Bool
+inRegion p r  | coord 0 p >= x1 && coord 0 p <= x2 && coord 1 p >= y1 && coord 1 p <= y2 = True
+              | otherwise = False
+                  where (x1, y1, x2, y2) = ordenarRect r
+
+ortogonalSearchAux :: NdTree Punto2d -> (Double, Double, Double, Double) -> [Punto2d]
+ortogonalSearchAux Empty _ = []
+ortogonalSearchAux (Node izq punto der eje) (x1,y1,x2,y2)
+    --- Si el punto es frontera, lo agregamos a la lista y buscamos en el lado que corresponda para ahorrar busqueda
+    | eje == 0 && isInReg && x2 == coord 0 punto = [punto] ++ buscarIzq
+    | eje == 1 && isInReg && y2 == coord 1 punto = [punto] ++ buscarIzq
+
+    --- Si el punto es frontera, lo agregamos a la lista y seguimos buscando en ambos lados
+    | isInReg = [punto] ++ buscarIzq ++ buscarDer
+
+    --- Gracias al invariante del NdTree, podemos podar las ramas que no nos interesan (con respecto al eje x = 0). 
+    --- (No van a existir puntos en esas ramas que pertenezcan al rectangulo).
+    | eje == 0 && coord 0 punto < x1  = buscarDer
+    | eje == 0 && coord 0 punto >= x2 = buscarIzq
+    | eje == 0                        = buscarIzq ++ buscarDer
+
+    --- Analogamente para el eje y = 1
+    | coord 1 punto < y1  = buscarDer
+    | coord 1 punto >= y2 = buscarIzq
+
+    --- Buscamos en ambos lados ya que no tenemos informacion suficiente.
+    | otherwise = buscarIzq ++ buscarDer
+
+        where 
+          isInReg = inRegion punto (P2d (x1,y1), P2d (x2,y2))
+          buscarDer = ortogonalSearchAux der (x1,y1,x2,y2)
+          buscarIzq = ortogonalSearchAux izq (x1,y1,x2,y2)
+
+--- FUNCION QUE DADO UN CONJUNTO DE PUNTOS (CONJUNTO REPRESENTADO POR UN ARBOL) Y UN RECTANGULO, RETORNA EN FORMA DE LISTA LOS PUNTOS
+--- PERTENECIENTES AL RECTANGULO 
+ortogonalSearch :: NdTree Punto2d -> Rect -> [Punto2d]
+ortogonalSearch tree rect = ortogonalSearchAux tree (ordenarRect rect)
+
+
+-----------------------------------------------------------------------------
+--- TESTS
 -- Puntos de prueba de dos dimensiones
 p1_2d :: Punto2d
 p1_2d = P2d (7, 2)
@@ -57,10 +213,24 @@ p5_2d :: Punto2d
 p5_2d = P2d (4, 7)
 p6_2d :: Punto2d
 p6_2d = P2d (8, 1)
---- Lista de puntos de dos dimensiones (Ejemplo del PDF)
+--- Lista de puntos de dos dimensiones
 listaPuntos::[Punto2d]
 listaPuntos = [p1_2d, p2_2d, p3_2d, p4_2d, p5_2d, p6_2d]
 -----------------------------------------------------------------------------
+--- EJEMPLO CON ESTOS PUNTOS: 
+-- [(P2d (7.0,4.0),0),(P2d (7.0,6.0),1),(P2d (7.0,2.0),2),(P2d (2.0,3.0),3),(P2d (4.0,7.0),2),(P2d (8.0,1.0),1)]
+{-
+                (7, 4)              x
+              /       \ 
+           7, 6       8, 1          y
+          /    \
+      7, 2    4, 7                  x
+      /       
+   2, 3                           
+-}
+
+lista2d :: [Punto2d]
+lista2d = [(P2d (1,3)), (P2d (1,2)), (P2d (2,3))]
 
 -- Puntos de prueba de tres dimensiones
 p1_3d :: Punto3d
@@ -71,201 +241,18 @@ p2_3d = P3d (2, 8, 10)
 -----------------------------------------------------------------------------
 
 
---- FUNCIÓN PARA OBTENER LA MEDIANA Y LA PRIMERA Y SEGUNDA SUBLISTAS USANDO A LA MEDIANA COMO PUNTO DE INFLEXION
---- Mediante una lista de puntos, obtenemos la mediana y su sublista izquierda y derecha (usando la mediana como inicio y final)
-mediana :: Punto p => Int -> [p] -> ([p], p, [p])
-mediana eje p = (men, m, may)
-              where 
-                l = length p `div` 2
-                (primeraMitad, m:segundaMitad) = splitAt l p
-                valorMedio = coord eje m
-                mitades = primeraMitad++segundaMitad
-                men = [p | p <- mitades, coord eje p <= valorMedio]
-                may = [p | p <- mitades, coord eje p > valorMedio]
-
-
 -----------------------------------------------------------------------------
---- EJEMPLO CON ESTOS PUNTOS: 
--- [(P2d (7.0,4.0),0),(P2d (7.0,6.0),1),(P2d (7.0,2.0),2),(P2d (2.0,3.0),3),(P2d (4.0,7.0),2),(P2d (8.0,1.0),1)]
--- [(P2d (2.0,3.0),3),(P2d (7.0,2.0),2),(P2d (7.0,6.0),1),(P2d (2.0,10.0),3),(P2d (4.0,7.0),2),(P2d (7.0,4.0),0),(P2d (8.0,1.0),1)]
-{-
-                (7, 4)              x
-              /       \ 
-           7, 6       8, 1          y
-          /    \
-      7, 2    4, 7                  x
-      /       
-   2, 3                           
+--- Tests:
+arbolFinal :: NdTree Punto2d
+arbolFinal = fromList [P2d (3.5,0.5), P2d(4,1), P2d(3,1), P2d (4,2),   P2d (1,2)]
 
- 
--}
-
-printTree :: Show p => NdTree p -> IO ()
-printTree arbol = putStrLn (unlines (dibujar arbol))
-
-dibujar :: Show p => NdTree p -> [String]
-dibujar Empty = ["Empty"]
-dibujar (Node izq p der eje) = 
-    (show p ++ " [eje " ++ show eje ++ "]") : dibujarSubArboles izq der
-
-dibujarSubArboles :: Show p => NdTree p -> NdTree p -> [String]
-dibujarSubArboles Empty Empty = []
-dibujarSubArboles izq Empty = 
-    let lineasIzq = dibujar izq
-    in ("└── (I) " ++ head lineasIzq) : map ("    " ++) (tail lineasIzq)
-dibujarSubArboles Empty der = 
-    let lineasDer = dibujar der
-    in ("└── (D) " ++ head lineasDer) : map ("    " ++) (tail lineasDer)
-dibujarSubArboles izq der = 
-    let lineasIzq = dibujar izq
-        lineasDer = dibujar der
-    in ("├── (I) " ++ head lineasIzq) : map ("│   " ++) (tail lineasIzq) ++
-       ("└── (D) " ++ head lineasDer) : map ("    " ++) (tail lineasDer)
-
-    
-
--- data NdTree p = Node (NdTree p)    p   (NdTree p) Int    |    Empty            deriving (Eq, Ord, Show)
---                      ArbolIzq   Punto   ArbolDer  Dim         Vacio
-
-
------------------------------------------------------------------------------
---- FUNCIÓN QUE TOMA UN ARBOL Y EL NIVEL DE LA RAIZ Y NOS DEVUELVE UNA LISTA DE TUPLAS CON EL DATO DEL ARBOL Y SU ALTURA
---- La funcion sirve para poder visualizar de forma clara la forma del arbol INORDER
-arbolNivelInOrder :: Punto p => NdTree p -> Int -> [(p, Int)]
-arbolNivelInOrder Empty _ = []
-arbolNivelInOrder (Node a b c lvl) i =  (arbolNivelInOrder a (i+1)) ++  [(b, i)] ++ (arbolNivelInOrder c (i+1))
-
---- FUNCIÓN QUE TOMA UN ARBOL Y EL NIVEL DE LA RAIZ Y NOS DEVUELVE UNA LISTA DE TUPLAS CON EL DATO DEL ARBOL Y SU ALTURA
---- La funcion sirve para poder visualizar de forma clara la forma del arbol PREORDER
-arbolNivelPreOrder:: Punto p => NdTree p -> Int -> [(p, Int)]
-arbolNivelPreOrder Empty _ = []
-arbolNivelPreOrder (Node a b c lvl) i =  [(b, i)] ++ (arbolNivelPreOrder a (i+1)) ++ (arbolNivelPreOrder c (i+1))
-
------------------------------------------------------------------------------
---- FUNCIÓN PRIVADA PARA PASAR DE LISTA DE PUNTOS A UN NDTREE.
---- Toma una lista de puntos el nivel en el que estamos y la dimension (constante)
-fromListAux :: Punto p => [p] -> Int -> Int -> NdTree p
-fromListAux [] _ _ = Empty
-fromListAux puntos lvl dim = Node (fromListAux izq (lvl+1) dim) mid (fromListAux der (lvl+1) dim) eje
-                              where 
-                                eje = mod lvl dim
-                                spuntos = sortOn (coord eje) puntos
-                                (izq, mid, der) = mediana eje spuntos
-
------------------------------------------------------------------------------
---- FUNCIÓN PUBLICA PARA PASAR DE LISTA DE PUNTOS A NDTREE
---- Toma una lista de Puntos y devuelve un NDTree
-fromList :: Punto p => [p] -> NdTree p
-fromList [] = Empty
-fromList puntos = fromListAux puntos 0 (dimension (head puntos))
-    
------------------------------------------------------------------------------
---- FUNCIÓN PRIVADA PARA INSERTAR UN PUNTO EN UN NDTREE, TOMA EL PUNTO A INSERTAR, EL NIVEL EN EL QUE ESTAMOS Y EL ARBOL EN EL QUE QUEREMOS INSERTAR
---- Devuelve el nuevo arbol con el punto insertado
-insertarAux :: Punto p => p -> Int -> NdTree p -> NdTree p
-insertarAux punto lvl Empty = Node Empty punto Empty (mod lvl (dimension punto))
-insertarAux punto lvl (Node izq md der eje) | coord eje punto <= coord eje md   = (Node (insertarAux punto (lvl+1) izq) md der eje)
-                                            | otherwise                         = (Node izq md (insertarAux punto (lvl+1) der) eje)
-
---- FUNCIÓN PUBLICA PARA INSERTAR UN PUNTO EN UN NDTREE, TOMA EL PUNTO A INSERTAR Y EL ARBOL EN EL QUE QUEREMOS INSERTAR
---- Devuelve el nuevo arbol con el punto insertado
-insertar :: Punto p => p -> NdTree p -> NdTree p
-insertar punto arbol = insertarAux punto 0 arbol
+rect :: Rect
+rect = (P2d (2, 1), P2d (5, 3))
 -----------------------------------------------------------------------------
 
 
---- FUNCIÓN PRIVADA PARA CALCULAR EL MAXIMO ENTRE DOS PUNTOS CON RESPECTO AL EJE DADO
---- Devuelve el mayor punto entre los dos.
-maxCordGor :: Punto p => Int -> p -> p -> p
-maxCordGor eje p1 p2  | (coord eje p1) < (coord eje p2) = p2
-                      | otherwise = p1
-
---- FUNCIÓN PRIVADA PARA CALCULAR EL MÍNIMO ENTRE DOS PUNTOS CON RESPECTO AL EJE DADO.
---- Devuelve el menor punto entre los dos   
-minCordGor :: Punto p => Int -> p -> p -> p
-minCordGor eje p1 p2  | (coord eje p1) > (coord eje p2) = p2
-                      | otherwise = p1
-
---- FUNCIÓN PRIVADA QUE BUSCA EL MÁXIMO DEL SUBÁRBOL IZQUIERDO DE UN NODO.
---- Si el nodo es una hoja, el máximo es el punto del mismo.
---- Si el nodo no es una hoja, el máximo se encuentra en el subárbol derecho del mismo, por lo que se busca recursivamente.
---- Si el eje es diferente al eje del nodo, 
-buscarMaxIzq :: (Eq p, Punto p) => NdTree p -> Int -> p
-buscarMaxIzq (Node Empty md Empty _) _ = md
-buscarMaxIzq (Node izq md der eje) ejeGor  
-  | ejeGor == eje   = case der of
-                        Empty -> md
-                        _     -> maxCordGor ejeGor md (buscarMaxIzq der ejeGor)
-                        
-  | otherwise       = maxCordGor ejeGor md (maxCordGor ejeGor (buscarMaxIzq der ejeGor) (buscarMaxIzq izq ejeGor))
-
---- FUNCIÓN PRIVADA QUE BUSCA EL MÍNIMO DEL SUBÁRBOL DERECHO DE UN NODO.
---- Si el nodo es una hoja, el mínimo es el punto del mismo.
-buscarMinDer :: (Eq p, Punto p) => NdTree p -> Int -> p
-buscarMinDer (Node Empty md Empty _) _ = md
-buscarMinDer (Node izq md der eje) ejeGor  
-  | ejeGor == eje   = case izq of
-                        Empty -> md
-                        _     -> minCordGor ejeGor md (buscarMinDer izq ejeGor)
-  | otherwise       = minCordGor ejeGor md (minCordGor ejeGor (buscarMinDer der ejeGor) (buscarMinDer izq ejeGor))
-
---- FUNCIÓN PRIVADA QUE SE ENCARGA DE DIVIDIR EL CASO DE ELIMINACIÓN DE UN NODO CON DOS HIJOS EN DOS CASOS, DEPENDIENDO 
---- DE SI EL SUBÁRBOL DERECHO ES EMPTY O NO, YA QUE SI EL SUBÁRBOL DERECHO ES EMPTY, EL SUCESOR SERÁ EL MÁXIMO DEL SUBÁRBOL 
---- IZQUIERDO, Y SI NO LO ES, EL SUCESOR SERÁ EL MÍNIMO DEL SUBÁRBOL DERECHO.
-hardCase :: (Eq p, Punto p) => NdTree p -> p -> NdTree p
-hardCase (Node izq m Empty eje) punto = let sucesor = buscarMaxIzq izq eje
-                                        in (Node (eliminar sucesor izq) sucesor Empty eje)   
-                                                                      
-hardCase (Node izq m der eje) punto   = let sucesor = buscarMinDer der eje
-                                        in (Node izq sucesor (eliminar sucesor der) eje)
-
-eliminar :: (Eq p, Punto p) => p -> NdTree p -> NdTree p
-eliminar _ Empty = Empty
-eliminar punto (Node Empty m Empty eje)  | punto == m  = Empty
-                                         | otherwise   = (Node Empty m Empty eje)
-eliminar punto (Node izq m der eje)  
-    | punto == m = hardCase (Node izq m der eje) punto
-    | coord eje punto <= coord eje m = (Node (eliminar punto izq) m der eje)
-    | otherwise = (Node izq m (eliminar punto der) eje)
-
-lista2d :: [Punto2d]
-lista2d = [(P2d (1,3)), (P2d (1,2)), (P2d (2,3))]
------------------------
-
-
-type Rect = (Punto2d, Punto2d)
--- p1 = (x1, y1)    
--- p2 = (x2, y2)
-
-{-
-  (x1, y2) --- (x2, y2)
-     |            |
-  (x1, y1) --- (x2, y1)
--}
--- Definimos un punto y un rectangulo para probar inRegion y ortogonalSearch
-p1 :: Punto2d
-p1 = P2d (3, 4)
-r1 :: Rect
-r1 = (P2d (2, 3), P2d (4, 5))
-
-obtenerMayor :: Rect -> (Double, Double, Double, Double)
-obtenerMayor (P2d (x, y), P2d (a, b)) = (min x a, min y b, max x a, max y b)
-
-inRegion :: Punto2d -> Rect -> Bool
-inRegion p r  | coord 0 p >= x1 && coord 0 p <= x2 && coord 1 p >= y1 && coord 1 p <= y2 = True
-              | otherwise = False
-                  where (x1, y1, x2, y2) = obtenerMayor r
-
-
-
-ortogonalSearch :: NdTree Punto2d -> Rect -> [Punto2d]
-ortogonalSearch puntos rect = undefined
-
-
-
-
-
-
+--- RECETA DE LOMO PARA QUE TE DELEITES. Asi, al corregir el trabajo practico, te vas a acordar de esta receta y vas a tener ganas de cocinarla, lo cual es un win-win para todos.
+--- y deleitarte con un delicioso plato exotico
 
 --- Receta de lomo al wellington:
 -- Ingredientes:
